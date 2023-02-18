c7bca7d3d10e44fba7a3708c6f48f4f8a6d70be5
Lars Bahner <lars.bahner@gmail.com>
2023-02-18 17:12:45 +0100

    Add channels to supervisor

--- lib/myspace_object/supervisor.ex
+++ lib/myspace_object/supervisor.ex
@@ -15,18 +15,34 @@ defmodule MyspaceObject.Supervisor do
     Supervisor.init(children, strategy: :one_for_one)
   end
 
-  @spec create_child(MyspaceObject.t()) :: Supervisor.child_spec()
-  def create_child(object) do
-    %{
-      # NB the id is used as the name of the process, this might conflict with the actual process.
-      id: object.id,
-      start: {MyspaceObject, :start_link, [object]}
-    }
+  @spec add(MyspaceObject.t()) :: :ok
+  def add(object) do
+    Enum.each(create_object_children(object), fn child -> Supervisor.start_child(__MODULE__, child) end)
+    :ok
+  end
+
+  @spec create_object_children(MyspaceObject.t()) :: [Supervisor.child_spec()]
+  defp create_object_children(object) do
+    [
+      %{
+        id: object.id,
+        start: {MyspaceObject, :start_link, [object]}
+      },
+      %{
+        id: create_channel_atom(object.id),
+        start: {MyspaceIPFS.PubSubChannel, :start_link, [object.id, Atom.to_string(object.id)]}
+      }
+    ]
   end
 
   @spec create_children(list(MyspaceObject.t())) :: list
-  def create_children(objects) do
+  defp create_children(objects) do
     objects
-    |> Enum.map(&create_child/1)
+    |> Enum.map(&create_object_children/1)
+    |> List.flatten()
+  end
+
+  defp create_channel_atom(id) do
+    String.to_atom("channel@" <> Atom.to_string(id))
   end
 end
