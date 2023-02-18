566968c687bf92b337e20f20276f4510cd88aa41
Lars Bahner <lars.bahner@gmail.com>
2023-02-18 16:15:28 +0100

    Delete unused state

--- lib/myspace_object/state.ex
+++
@@ -1,7 +0,0 @@
-defmodule MyspaceObject.State do
-  @moduledoc """
-  The state of a MyspaceObject. State is read from and written to IPFS.
-  """
-
-  # FIXME: create an Agent to hold the state, and use that instead of a GenServer.
-end
