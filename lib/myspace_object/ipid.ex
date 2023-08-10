85df9156f6aa5a160e140c307173b2d96c77605d
Lars Bahner <lars.bahner@gmail.com>
2023-08-10 02:14:01 +0200

    WIP: ipid

--- lib/myspace_object/ipid.ex
+++ lib/myspace_object/ipid.ex
@@ -1,8 +1,6 @@
 defmodule MyspaceObject.Ipid do
   @moduledoc false
 
-  use GenServer
-
   require Logger
   import MyspaceObject.Utils
 
@@ -35,53 +33,36 @@ defmodule MyspaceObject.Ipid do
 
   @typep object :: MyspaceObject.t()
 
-  @spec start_link(t) :: :ignore | {:error, any} | {:ok, pid}
-  def start_link(ipid) do
-    GenServer.start(__MODULE__, ipid, name: via_tuple(ipid.id))
-  end
-
-  @spec init(map) :: {:ok, nil}
-  def init(state), do: {:ok, state}
-
   @doc """
   Create a new IPID from a MyspaceObject.
   """
   @spec new(object) :: {:ok, t()}
   def new(object) when is_struct(object) do
-    {:ok, %__MODULE__{
-      id: did_gen!(object),
-      context: @context,
-      public_key: object.public_key.pem,
-      created: object.created,
-      updated: now()
-    }}
+    {:ok,
+     %__MODULE__{
+       id: did_gen!(object),
+       context: @context,
+       public_key: object.public_key.pem,
+       created: object.created,
+       updated: now()
+     }}
   end
 
   @doc """
   Publish the IPID to IPFS. Returns :ok here, and then delivers the ipid
   back to the calling process, in a {:ipid_publish, MySpaceObject.ipid()} message.
   """
-  @spec publish(object) :: :ok
-  def publish(object) do
-    # This is a pipeline. Send it to add, which will then pass it to publish.
-    # ipid_add wll never need to be called directly. Use ExIpfs.add directly
-    # for that.
-    GenServer.cast(via_tuple(object.id), {:ipid_publish, object})
+  @spec publish(t) :: :ok
+  def publish(ipid) do
+    Logger.debug("Publishing IPID #{ipid.id} to IPFS")
+    start = Time.utc_now()
+
+    key = did_fragment!(ipid.id)
+    {:ok, add_result} = ExIpfs.add(json_encode_struct(ipid))
+
+    ExIpfsIpns.publish(add_result.hash, key: key)
+    Logger.debug("Publication of IPID finished in #{seconds_since(start)} seconds")
     :ok
-  end
-
-  def handle_cast({:ipid_publish, object}, state) do
-    state = %__MODULE__{state | updated: now(), public_key: object.public_key.pem}
-    ipid_publish(state)
-    {:noreply, state}
-  end
-
-  def handle_cast(_, state) do
-    {:noreply, state}
-  end
-
-  def handle_info(_, state) do
-    {:noreply, state}
   end
 
   @doc """
@@ -115,21 +96,5 @@ defmodule MyspaceObject.Ipid do
   @spec did_gen!(MyspaceObject.t()) :: binary()
   def did_gen!(object) when is_binary(object.id) and is_binary(object.ipns) do
     "did:ipid:" <> object.ipns <> "#" <> object.id
-  end
-
-  defp ipid_publish(ipid) when is_struct(ipid) do
-    Logger.debug("Publishing IPID #{ipid.id} to IPFS")
-    start = Time.utc_now()
-
-    key = did_fragment!(ipid.id)
-    {:ok, add_result} = ExIpfs.add(json_encode_struct(ipid))
-
-    ExIpfsIpns.publish(add_result.hash, key: key)
-    Logger.debug("Publication of IPID finished in #{seconds_since(start)} seconds")
-    :ok
-  end
-
-  defp via_tuple(name, registry \\ @registry) do
-    {:via, Registry, {registry, name}}
   end
 end
